c35bd0eeb8898d78ad709e024a1bb29ea20a29cc
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-09-09 01:43:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-   "SELECT books.title, books.year FROM books
+  "SELECT books.title, books.year FROM books
     INNER JOIN series
       ON books.series_id = series.id
     WHERE series.id = 1
@@ -7,14 +7,13 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-   "SELECT name, motto FROM characters
+  "SELECT name, motto FROM characters
     ORDER BY length(motto) DESC
     LIMIT 1;"
 end
 
-
 def select_value_and_count_of_most_prolific_species
-   "SELECT species, COUNT(species) AS num_species FROM characters
+  "SELECT species, COUNT(species) AS num_species FROM characters
     GROUP BY species
     ORDER BY num_species DESC
     LIMIT 1;"
