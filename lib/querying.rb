5f6597eb902796be6a67898da592d3e70ab59ed3
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-09-09 01:40:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+   "SELECT books.title, books.year FROM books
+    INNER JOIN series
+      ON books.series_id = series.id
+    WHERE series.id = 1
+    ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
