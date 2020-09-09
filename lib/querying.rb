369ed9db28562cc8214e5a5a40a2e921e560b01a
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-09-09 01:41:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name AS author_name, subgenres.name AS subgenre_name FROM series
+    INNER JOIN authors
+      ON series.author_id = authors.id
+    INNER JOIN subgenres
+      ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
