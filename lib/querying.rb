6b0d554688f924fabba9e14ee14bb341233a0063
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-09-09 01:41:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,15 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM (
+    SELECT series.title AS title, COUNT(characters.series_id) AS num_species
+      FROM series
+      INNER JOIN characters
+        ON series.id = characters.series_id
+      WHERE characters.species = 'human'
+      GROUP BY characters.series_id
+      ORDER BY num_species DESC
+      LIMIT 1);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
