97a3174a08cae981ea076d780addd2fd9456218a
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-09-09 01:40:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+   "SELECT name, motto FROM characters
+    ORDER BY length(motto) DESC
+    LIMIT 1;"
 end
 
 
